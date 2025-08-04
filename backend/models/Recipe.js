9a2f0cc868ee7d814673fe5765634cb635243893
Sathish Raja <dareakl@users.noreply.github.com>
2025-08-05 00:45:41 +0800

    user,recipe model created

---
+++ backend/models/Recipe.js
@@ -0,0 +1,45 @@
+import { request } from "express";
+import mongoose from "mongoose";
+import User from "./User";
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    ingredients: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    instructions: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+      required: true,
+    },
+    cookingTime: {
+      type: Number,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+export default Recipe;
