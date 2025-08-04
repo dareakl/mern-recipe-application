1227254c1b6f459598d4e495100b50ef8baddf73
Sathish Raja <dareakl@users.noreply.github.com>
2025-08-05 01:14:01 +0800

    recipe create,get,update,delete routes added

---
+++ backend/routes/recipes.js
@@ -0,0 +1,101 @@
+import express from "express";
+import Recipe from "../models/Recipe.js";
+import Recipe from "../models/Recipe.js";
+
+const router = express.Router();
+
+//Create recipe
+router.post("/", async (req, res) => {
+  const { title, ingredients, instructions, category, photoUrl, cookingTime } =
+    req.body;
+
+  try {
+    if (
+      !title ||
+      !ingredients ||
+      !instructions ||
+      !category ||
+      !photoUrl ||
+      !cookingTime
+    ) {
+      return res.status(400).json({ message: "Please Fill all fields" });
+    }
+
+    const recipe = await Recipe.create({
+      title,
+      ingredients,
+      instructions,
+      category,
+      photoUrl,
+      cookingTime,
+    });
+    res.status(200).json(recipe);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Get recipes
+router.get("/", async (req, res) => {
+  const { category } = req.query;
+  try {
+    const query = category ? { category } : {};
+    const recipes = await Recipe.find(query);
+    res.json(recipes);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Get single recipe
+router.get("/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe Not Found" });
+    }
+    res.json(recipe);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Update a recipe
+router.put("/id", async (req, res) => {
+  const { title, ingredients, instructions, category, photoUrl, cookingTime } =
+    req.body;
+
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({ message: "REcipe Not Found" });
+    }
+    recipe.title = title || recipe.title;
+    recipe.ingredients = ingredients || recipe.ingredients;
+    recipe.instructions = instructions || recipe.instructions;
+    recipe.category = category || recipe.category;
+    recipe.photoUrl = photoUrl || recipe.photoUrl;
+    recipe.cookingTime = cookingTime || recipe.cookingTime;
+
+    const updatedRecipe = await recipe.save();
+    res.json(updatedRecipe);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//Delete recipe
+router.delete("/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) {
+      return res.status(404).json({ message: "REcipe Not Found" });
+    }
+    await recipe.deleteOne();
+    res.json({ message: "Recipe Deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
