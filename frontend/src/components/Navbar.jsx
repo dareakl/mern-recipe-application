2f79db60392ea55e54a6e778e33706f35d7665d8
Sathish Raja <dareakl@users.noreply.github.com>
2025-08-06 18:30:54 +0800

    login and logout frontend functionality added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
   return (
     <nav className="bg-white shadow-md p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
@@ -10,12 +17,25 @@ const Navbar = () => {
         </Link>
 
         <div className="flex gap-x-4">
-          <Link to="/login">
-            <button>Login</button>
-          </Link>
-          <Link to="/register">
-            <button>Register</button>
-          </Link>
+          {user ? (
+            <div>
+              <button
+                onClick={handleLogout}
+                className="text-gray-600 hover:text-gray-800"
+              >
+                Logout
+              </button>
+            </div>
+          ) : (
+            <>
+              <Link to="/login">
+                <button>Login</button>
+              </Link>
+              <Link to="/register">
+                <button>Register</button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
