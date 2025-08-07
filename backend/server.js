c62b01c976f14f6a2c995133a7dfee99e00e336c
Sathish Raja <dareakl@users.noreply.github.com>
2025-08-07 15:19:54 +0800

    production deployment code added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import recipesRoutes from "./routes/recipes.js";
 import { connectDB } from "./config/db.js";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 5002;
 
@@ -14,6 +15,15 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/recipes", recipesRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
